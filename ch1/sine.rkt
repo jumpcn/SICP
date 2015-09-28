8d8614c07d3a504d9473793176f84444b5b369bc
jumpcn <572519236@qq.com>
2015-09-28 22:31:04 +0800

    增加一些模块

---
+++ ch1/sine.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (cube x) (* x x x))
+
+(define (p x) (- 3 x) (* 4 (cube x)))
+
+(define (abs x)
+  (if (> x 0)
+      x
+      (- 0 x)))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+           angle
+           (p (sine (/ angle 3.0)))))
+
