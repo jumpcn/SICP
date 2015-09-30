ecbe3fb18d115b015cfec8d3c1b7dca4c6b1c833
jumpcn <572519236@qq.com>
2015-09-30 11:39:33 +0800

    2.19 complete

--- ch2/2.19.rkt
+++ ch2/2.19.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
 (define (cc amount coin-values)
-  (cond ((= amout 0) 1)
-        ((or (< amout 0) (no-more? coin-values)) 0)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
         (else
          (+ (cc amount
-                (expect-first-denomation coin-values))
+                (expect-first-denomination coin-values))
             (cc (- amount
                    (first-denomination coin-values))
                 coin-values)))))
@@ -13,11 +13,11 @@
 (define (no-more? coin-values)
   (null? coin-values))
 
-(define (first-denomination )
+(define (first-denomination a)
   (car a))
 (define (expect-first-denomination a)
   (cdr a))
 
 (define us-coins (list 50 25 10 5 1))
   
-(count-change 100)
+(cc 100 us-coins)
