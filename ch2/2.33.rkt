7cde7fd0a6b2cf0ec52dc689d6ab1c1673a38cce
ShuiNiu <572519236@qq.com>
2016-01-05 00:49:46 +0800

    Update 2.33.rkt
    (list ) is an empty list but (list null) is not

--- ch2/2.33.rkt
+++ ch2/2.33.rkt
@@ -14,15 +14,11 @@
 
 (define (length seq)
   (accumulate (lambda (x y)
-                (+ (if (null? x)
-                       0
-                       1)
-                   y))
+                (+ 1 y))
                 0 seq))
 
-(length (list null))
+(length (list ))
 (length (list 2 3 4))
 (length (list 1 2 3 (list 4)))
 (ap (list 1 2 3) (list 4 5 6))
 (map (lambda (n) (* n 2)) (list 1 2 3 4))
-
