2565ed3fec9495f774337e6f55f4b83ed5a21a2f
jumpcn <572519236@qq.com>
2016-03-30 12:47:40 +0800

    Flush

---
+++ ch2/2.21.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (square-list items)
+  (if (empty? items)
+      null
+      (cons (((lambda(n) (* n n)) (car items)) (square-list (cdr items))))))
+
+(square-list (list 1 2 3 4))
+
+
+      
